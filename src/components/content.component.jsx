c2569754cd5edd1e53a50b6f98400ef73f7e4e91
shilpashingnapure <shingnapureshilpa2@gmail.com>
2024-01-09 22:20:45 +0530

    dashboard

---
+++ src/components/content.component.jsx
@@ -0,0 +1,146 @@
+import "./content.component.scss";
+function Content() {
+  const data = [
+    {
+      orderId: "#281209",
+      orderDate: "7 July , 2023",
+      orderAmount: "1,278.23",
+      transactionFees: "22",
+    },
+    {
+      orderId: "#281209",
+      orderDate: "7 July , 2023",
+      orderAmount: "1,278.23",
+      transactionFees: "22",
+    },
+    {
+      orderId: "#281209",
+      orderDate: "7 July , 2023",
+      orderAmount: "1,278.23",
+      transactionFees: "22",
+    },
+    {
+      orderId: "#281209",
+      orderDate: "7 July , 2023",
+      orderAmount: "1,278.23",
+      transactionFees: "22",
+    },
+    {
+      orderId: "#281209",
+      orderDate: "7 July , 2023",
+      orderAmount: "1,278.23",
+      transactionFees: "22",
+    }
+  ];
+  return (
+    <div className="content">
+      <div className="header-section">
+        <div>
+          Payments
+          <small>
+            <img src="images/Help.svg" alt=""/>
+            How it works
+          </small>
+        </div>
+        <div className="search-input">
+          <img src="images/search.png" alt="no img" />
+          <input type="text" placeholder="Search features, tutorials, etc." />
+        </div>
+        <div className="header-icons">
+          <div>
+            <img src="images/chat-icon.png" alt="" />
+          </div>
+          <div>
+            <img src="images/down-arrow.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="overview-section">
+        <div className="overview-header">
+          <h3 className="heading">Overview</h3>
+          <div>
+            <img src="./images/btn.svg" alt=""/>
+          </div>
+        </div>
+
+        <div className="overview-content">
+          <div>
+            <span>Online orders</span>
+            <h3>231</h3>
+          </div>
+          <div>
+            <span>Amount received</span>
+            <h3>₹23,92,312.19</h3>
+          </div>
+        </div>
+      </div>
+      <div className="transaction-section">
+        <h3 className="heading">Transactions | This Month</h3>
+        <div>
+          <div className="filter-section">
+            <div className="search-input">
+              <img src="./images/search.png" alt="" />
+              <input type="text" placeholder="Search by order ID..." />
+            </div>
+            <div className="btns">
+              <img src="./images/Sort.svg" alt="" />
+              <img src="./images/download.svg" alt=""/>
+            </div>
+          </div>
+
+          <div className="table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Order ID</th>
+                  <th className="flex">
+                    Order date{" "}
+                    <img src="/images/down-arrow.png" width="10px" alt="" />
+                  </th>
+                  <th>Order amount</th>
+                  <th className="flex">
+                    Transaction fees <img src="/images/info.svg" alt="" />
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+
+                {
+                    data.map((element , index) => {
+                       return  <tr key={index}>
+                            <td>{ element.orderId}</td>
+                            <td>{element.orderDate}</td>
+                            <td>₹{element.orderAmount}</td>
+                            <td>₹{element.transactionFees}</td>
+                        </tr>
+                        
+                    })
+                }
+              </tbody>
+            </table>
+            <div className="table-btn">
+                <div className="button">
+                <img src="./images/left-arrow.svg" alt=""/>
+
+                   <span>Previous</span> 
+                </div>
+                <div className="page-num">
+                    <span className="active-pagination">1</span>
+                    <span>2</span>
+                    <span>3</span>
+                    <span>4</span>
+                    </div>
+                <div className="button">
+                    <span>Next</span>
+                    <img src="./images/right-arrow.svg" alt=""/>
+
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/nav.component.jsx
@@ -0,0 +1,80 @@
+import "./nav.component.scss";
+function Navbar() {
+  return (
+    <div className="nav">
+      <div className="header">
+        <div className="logo">
+          <div>
+            <img src="image.svg" alt="" />
+            <div>
+              <h4>Nishyan</h4>
+              <span>Visit store</span>
+            </div>
+          </div>
+          <img src="./images/down-outline-arrow.svg" alt="" width='15px' height='15px'/>
+        </div>
+        <ul>
+          <li>
+            <img src="./icons/home.svg" alt="" />
+            <span>Home</span>
+          </li>
+          <li>
+            <img src="./icons/orders.svg" alt="" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <img src="./icons/product.svg" alt="" />
+            <span>Products</span>
+          </li>
+          <li>
+            <img src="./icons/delivery.svg" alt="" />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <img src="./icons/marketing.svg" alt="" />
+            <span>Marketing</span>
+          </li>
+          <li>
+            <img src="./icons/analytics.svg" alt="" />
+            <span>Analytics</span>
+          </li>
+          <li className="active">
+            <img src="./icons/payments.svg" alt="" />
+            <span>Payments</span>
+          </li>
+          <li>
+            <img src="./icons/tools.svg" alt="" />
+            <span>Tools</span>
+          </li>
+          <li>
+            <img src="./icons/discount.svg" alt="" />
+            <span>Discounts</span>
+          </li>
+          <li>
+            <img src="./icons/audience.svg" alt="" />
+            <span>Audience</span>
+          </li>
+          <li>
+            <img src="./icons/appearance.svg" alt="" />
+            <span>Appearance</span>
+          </li>
+          <li>
+            <img src="./icons/plugins.svg" alt="" />
+            <span>Plugins</span>
+          </li>
+        </ul>
+      </div>
+      <div className="footer">
+        <div>
+          <img src="./icons/wallet.svg" alt="" />
+        </div>
+        <div>
+          <span>Available creadits</span>
+          <h3>222.10</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
